0f4644cb2d1707a7f6f6de6acfa280838586d002
FranciscoMFR <franciscor@ua.pt>
2022-09-23 17:09:04 +0100

    function that collect the infos and for loop in all cards

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -55,6 +55,8 @@ print(f'reTweets: {reTweet}')
 likes = driver.find_element_by_xpath('//div[@data-testid="reply"]').text
 print(f'likes: {likes}')
 '''
+
+#Collecting tweets infos
 cards = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')                        #Save all tweets that page have loaded
 card = cards[0]                                                                                 #Choose the 1st tweet
 
@@ -70,12 +72,51 @@ print(f'time: {timeStamp}')
 tweetText = card.find_element_by_xpath('.//div[@data-testid="tweetText"]').text                 #Save the tweet text
 print(f'tweet text: {tweetText}')
 
-reply = card.find_element_by_xpath('.//div[@data-testid="reply"]').text                         #Save the tweet repply count
-print(f'reply: {reply}')    
+replyCnt = card.find_element_by_xpath('.//div[@data-testid="reply"]').text                      #Save the tweet repply count
+print(f'replyCnt: {replyCnt}')    
     
 reTweet = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text                     #Save the tweet retweet count
 print(f'retweets: {reTweet}')
 
 likes = card.find_element_by_xpath('.//div[@data-testid="like"]').text                          #Save the tweet likes count
 print(f'likes: {likes}')
+
+
+
+
+
+#function to extract all infos
+def getTweetData(card):                                                                         #This function grabs a single card and extract username, user@, timestamp, replu count, retweet count, like count and return a tuple witth the info                                          
+
+    username = card.find_element_by_xpath('.//span').text                                           
+    atUsername = card.find_element_by_xpath('.//span[contains(text(), "@")]').text 
+    try:
+        timeStamp = card.find_element_by_xpath('.//time').get_attribute('datetime') 
+    except NoSuchElementException:
+        return
+    tweetText = card.find_element_by_xpath('.//div[@data-testid="tweetText"]').text
+    replyCnt = card.find_element_by_xpath('.//div[@data-testid="reply"]').text
+    reTweetCnt = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text  
+    likesCnt = card.find_element_by_xpath('.//div[@data-testid="like"]').text
+
+    return (username, atUsername, timeStamp, tweetText, replyCnt, reTweetCnt, likesCnt)
+
+
+print('-------------Testing the function-------------')
+print(getTweetData(card))
+
+
+#for loop to catch all the tweets infos that have been saved
+tweetData=[]                                                                                    #This array will have all the tuples with the tweet infos
+
+for card in cards:                                                                              #This for loop iterates over all cards, produce the tuple infos and save them in tweetData array 
+    
+    data = getTweetData(card)
+    if data:
+        tweetData.append(data)
+
+print('-------------Testing the for loop-------------')
+print(tweetData[0])
+print(tweetData[1])
+print(tweetData[2])
 
