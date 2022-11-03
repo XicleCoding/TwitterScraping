61bbfe688b13144b86be76e6a9d343e66357e659
FranciscoMFR <franciscor@ua.pt>
2022-11-03 17:29:29 +0000

    same

--- scrap_the_twitter.py
+++ scrap_the_twitter.py
@@ -1,36 +1,52 @@
 from asyncore import write
+from cgitb import text
 import csv
 from getpass import getpass
 from lib2to3.pgen2 import driver
+from logging.config import valid_ident
 from optparse import Option
+from re import X
 from time import sleep
 import time
 import calendar
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from msedge.selenium_tools import Edge, EdgeOptions
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 #from twitter_scraper_things import getTweetData
 
 #function to extract all infos
 def getTweetData(card):                                                                         
 
-    username = card.find_element_by_xpath('.//span').text                                           
-    atUsername = card.find_element_by_xpath('.//span[contains(text(), "@")]').text 
+    #username = card.find_element_by_xpath('.//span').text    
+    username = card.find_element(by=By.XPATH, value='.//span').text                                       
+    #atUsername = card.find_element_by_xpath('.//span[contains(text(), "@")]').text 
+    atUsername = card.find_element(by=By.XPATH, value='.//span[contains(text(), "@")]').text
     try:
-        timeStamp = card.find_element_by_xpath('.//time').get_attribute('datetime') 
+        #timeStamp = card.find_element_by_xpath('.//time').get_attribute('datetime') 
+        timeStamp = card.find_element(by=By.XPATH, value='.//time').get_attribute('datetime') 
     except NoSuchElementException:                                                              
         return
-    tweetText = card.find_element_by_xpath('.//div[@data-testid="tweetText"]').text
-    replyCnt = card.find_element_by_xpath('.//div[@data-testid="reply"]').text                  
-    reTweetCnt = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text              
-    likesCnt = card.find_element_by_xpath('.//div[@data-testid="like"]').text                  
+    #tweetText = card.find_element_by_xpath('.//div[@data-testid="tweetText"]').text
+    tweetText = card.find_element(by=By.XPATH, value='.//div[@data-testid="tweetText"]').text
+    #replyCnt = card.find_element_by_xpath('.//div[@data-testid="reply"]').text                  
+    replyCnt = card.find_element(by=By.XPATH, value='.//div[@data-testid="reply"]').text
+    #reTweetCnt = card.find_element_by_xpath('.//div[@data-testid="retweet"]').text              
+    reTweetCnt = card.find_element(by=By.XPATH, value='.//div[@data-testid="retweet"]').text
+    #likesCnt = card.find_element_by_xpath('.//div[@data-testid="like"]').text 
+    likesCnt = card.find_element(by=By.XPATH, value='.//div[@data-testid="like"]').text         
 
     return (username, atUsername, timeStamp, tweetText, replyCnt, reTweetCnt, likesCnt)
 
 #Create instance of web driver
-options = EdgeOptions()
-options.use_chromium = True
-driver = Edge(options=options)
+#options = EdgeOptions()
+#options.use_chromium = True
+#driver = Edge(options=options)
+service = EdgeService(executable_path=EdgeChromiumDriverManager().install())
+driver = webdriver.Edge(service=service)
 
 #Navigate to login screen
 driver.get('https://twitter.com/login')
@@ -38,7 +54,8 @@ driver.maximize_window()
 sleep(5)
 
 #Enter the username/email
-username = driver.find_element_by_xpath('//input[@name="text"]')                                
+#username = driver.find_element_by_xpath('//input[@name="text"]')     
+username = driver.find_element(by=By.XPATH, value='//input[@name="text"]')                          
 username.send_keys('TwittyScrapy')                                                              
 username.send_keys(Keys.RETURN)                                                                 
 sleep(3)   
@@ -46,19 +63,22 @@ sleep(3)
 #Enter the password
 #my_password = getpass()
 my_password = 'masterthesis'
-password = driver.find_element_by_xpath('//input[@name="password"]')                            
+#password = driver.find_element_by_xpath('//input[@name="password"]')  
+password = driver.find_element(by=By.XPATH, value='//input[@name="password"]')                             
 password.send_keys(my_password)                                                              
 password.send_keys(Keys.RETURN)                                                                 
 sleep(3)  
 
 #Search term
-search_input = driver.find_element_by_xpath('//input[@aria-label="Consulta de busca"]')         
+#search_input = driver.find_element_by_xpath('//input[@aria-label="Consulta de busca"]') 
+search_input = driver.find_element(by=By.XPATH, value='//input[@aria-label="Consulta de busca"]')       
 search_input.send_keys('#Eleicoes2022')                                                              
 search_input.send_keys(Keys.RETURN)                                                            
 sleep(3)                                                                             
 
 #Navigate to historical 'Principais' tab
-driver.find_element_by_link_text('Principais').click()                                       
+#driver.find_element_by_link_text('Principais').click()    
+driver.find_element(by=By.LINK_TEXT, value='Principais').click()                                   
 sleep(3)
 
 #Get all tweets on the page
@@ -67,7 +87,8 @@ tweet_ids = set()
 last_position = driver.execute_script("return window.pageYOffset;")
 scrolling = True
 while scrolling:
-    page_cards = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
+    #page_cards = driver.find_elements_by_xpath('//article[@data-testid="tweet"]')
+    page_cards = driver.find_elements(by=By.XPATH, value='//article[@data-testid="tweet"]')
     for card in page_cards[-15:]:                                                               #Look ate the last 15 tweet because the continuous growing of the page
         tweet = getTweetData(card)
         if tweet:
---
+++ visualizer_run.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\User\OneDrive - Universidade de Aveiro\Desktop\UA\Tese\TwitterScraping\conda_activate_scraper.bat" && cd .. && cd twitter-explorer && streamlit run visualizer.py
+pause
