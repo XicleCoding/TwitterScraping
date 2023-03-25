79ae0d877f4a0839f549ecc02371325900c155ab
FranciscoMFR <franciscor@ua.pt>
2023-03-25 21:39:03 +0000

    fast func to format the data counters

--- fast_Tests.py
+++ fast_Tests.py
@@ -1,6 +1,22 @@
+'''
 aux = '70.4K'
-
 aux2 = aux.split('.')
 aux3 = aux2[1].split('K')
 final = aux2[0] + ',' + aux3[0] + '00'
 print(final)
+'''
+
+def countFormat(count):
+    if '.' in count:
+        split1 = count.split('.')
+        split2 = split1[1].split('K')
+        finalCount = split1[0] + ',' + split2[0] + '00'
+    else:
+        split1 = count.split('K')
+        finalCount = split1[0] + ',000'
+    return finalCount
+
+
+
+print(countFormat('70.4K'))
+print(countFormat('61K'))
